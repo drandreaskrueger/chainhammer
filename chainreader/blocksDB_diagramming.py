88a0ffb066d1b762eaac9ef7de3f8bc7af64c56c
drandreaskrueger <andreas.krueger@electron.org.uk>
2018-06-26 16:18:46 +0100

    refactored jupyter notebook into visualisation(jupyter), and functions(blocksDB_diagramming.py)

---
+++ chainreader/blocksDB_diagramming.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+"""
+@summary: for the jupyter notebooks: tools, column creators, diagramming routines, etc. 
+
+@version: v20 (26/June/2018)
+@since:   26/June/2018
+@organization: electron.org.uk
+@author:  https://github.com/drandreaskrueger
+@see: https://gitlab.com/electronDLT/chainhammer for updates
+"""
+
+################
+## Dependencies:
+
+import sqlite3
+import pandas
+import numpy
+import matplotlib
+
+
+################
+## DB queries
+
+def DB_query(SQL, conn):
+    """
+    any SQL query, with many answers 
+    """
+    cur = conn.cursor()
+    cur.execute(SQL)
+    result = cur.fetchall()
+    return result
+
+
+
+def DB_tableSize(tablename, conn):
+    """
+    prints number of rows
+    """
+    count = DB_query("SELECT COUNT(*) FROM %s" % tablename, conn)
+    print ("TABLE %s has %d rows" % (tablename, count[0][0]))
+    return count
+
+
+def maxBlockNumber(conn):
+    """
+    what is the first & last block we have?
+    """
+    result = DB_query("SELECT MIN(blocknumber), MAX(blocknumber) FROM blocks", conn)
+    print ("MIN(blocknumber), MAX(blocknumber) = %s " % (result) )
+    return result
+
+
+def check_whether_complete(blocknumbers):
+    """
+    do we have consecutive blocks, none missing?
+    """
+    start = min(blocknumbers)[0]
+    last = max(blocknumbers)[0]
+    old = start-1
+    total=0
+    for bn in blocknumbers:
+        bn = bn[0]
+        missing=bn-old-1
+        if missing>0:
+            print ("from ", old+1, "to", bn - 1, "there are ", missing, " missing")
+            total+=missing
+        old = bn
+    print()
+    print ("complete" if not total else "some %d blocks missing" % total, end=" ")
+    print ("between blocks %d and %d." %(min(blocknumbers)[0], max(blocknumbers)[0]))
+
+
+##################
+## add columns
+
+
+def add_blocktime(df):
+    """
+    blocktime = timestamp[n] - timestamp[n-1]
+    """
+    df['blocktime'] = df['timestamp'] - df['timestamp'].shift()
+    df.loc[1, "blocktime"] = numpy.nan
+
+
+def add_TPS(df, numBlocks):
+    """
+    transactions per second
+    with differently sized (rectangular) windows
+    """
+    name = 'TPS_%dblks'%numBlocks if numBlocks>1 else 'TPS_%dblk'%numBlocks
+    df[name]=df['txcount'].rolling(numBlocks).sum() / df['blocktime'].rolling(numBlocks).sum()
+    
+
+def add_GUPS(df, numBlocks):
+    """
+    gasUsed per second
+    """
+    name = 'GUPS_%dblks'%numBlocks if numBlocks>1 else 'GUPS_%dblk'%numBlocks
+    df[name]=df['gasUsed'].rolling(numBlocks).sum() / df['blocktime'].rolling(numBlocks).sum()
+
+def add_GLPS(df, numBlocks):
+    """
+    gasLimit per second
+    """
+    name = 'GLPS_%dblks'%numBlocks if numBlocks>1 else 'GLPS_%dblk'%numBlocks
+    df[name]=df['gasLimit'].rolling(numBlocks).sum() / df['blocktime'].rolling(numBlocks).sum()
+
+
+##################################################
+## diagramming
+##
+## TODO: also get the simpler step-by-step ones, 
+##       from the original blocksDB_analyze.ipynb
+##################################################
+
+def diagrams(df, blockFrom, blockTo, prefix="", gas_logy=True, bt_logy=True, tps_logy=False):
+    
+    # https://github.com/matplotlib/matplotlib/issues/5907#issuecomment-179001811
+    matplotlib.rcParams['agg.path.chunksize'] = 10000
+    
+    # prepare 2x2 subplots
+    plt = matplotlib.pyplot
+    fig, axes = plt.subplots(nrows=2, ncols=2,figsize=(15,10))
+    plt.tight_layout(pad=6.0, w_pad=6.0, h_pad=7.5)
+    title = prefix + " blocks %d to %d" % (blockFrom, blockTo)
+    plt.suptitle(title, fontsize=16)
+    
+    # bar charts are too expensive when too many blocks
+    numBlocks =  blockTo - blockFrom
+    kind = 'bar' if numBlocks<2000 else 'line'
+    
+    # TPS averages --> legend
+    cols=['TPS_1blk', 'TPS_3blks', 'TPS_5blks', 'TPS_10blks']
+    averages=df[cols][blockFrom:blockTo].mean()
+    legend = [col + " (av %.1f)" % averages[col] for col in cols]
+    # print (legend)
+    
+    # TPS diagram
+    cols = ['blocknumber'] + cols
+    ax=df[cols][blockFrom:blockTo].plot(x='blocknumber', rot=90, ax=axes[0,0],  logy=tps_logy)
+    ax.set_title("transactions per second")
+    ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    ax.legend(legend);
+    
+    # BT
+    ax=df[['blocknumber', 'blocktime']][blockFrom:blockTo].plot(x='blocknumber', 
+                                                                kind=kind, 
+                                                                ax=axes[0,1],
+                                                                logy=bt_logy)
+    ax.set_title("blocktime since last block")
+    ## TODO: how to reduce the number of ticks on the x-axis??? nbins=10 is not working correctly
+    # ax.locator_params(nbins=10, axis='x')
+    # xlength=blockTo+1-blockFrom 
+    # ticks = numpy.arange(blockFrom, blockTo+1, round(xlength/10))
+    # print (ticks)
+    
+        
+    # blocksize
+    ax=df[['blocknumber', 'size']][blockFrom:blockTo].plot(x='blocknumber', rot=90, kind=kind, ax=axes[1,0])
+    # ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    ax.get_yaxis().get_major_formatter().set_scientific(False)
+    ax.set_title("blocksize in bytes")
+    
+    # gas
+    ax=df[['blocknumber', 'GLPS_1blk', 'GUPS_1blk']][blockFrom:blockTo].plot(x='blocknumber', 
+                                                                             rot=90, ax=axes[1,1], 
+                                                                             logy=gas_logy)
+    ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    if not gas_logy:
+        ax.get_yaxis().get_major_formatter().set_scientific(False)
+    ax.set_title("gasUsed and gasLimit per second")
+    
+    # save diagram to PNG file
+    fig.savefig("img/%s_tps-bt-bs-gas_blks%d-%d.png" % (prefix,blockFrom,blockTo))
+
+    
+
+
+
+
+###############################################################################
+
+
+
+if __name__ == '__main__':
+    
+    print ("Don't start this directly. It is intended to be imported by the jupyer notebooks.")
