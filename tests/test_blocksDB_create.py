99d98e839730cc4d55172205024aa9b3367fb547
Andreas Krueger <coding@AndreasKrueger.de>
2018-12-02 17:24:41 +0000

    all tests done, tested on TestRPC

--- tests/test_blocksDB_create.py
+++ tests/test_blocksDB_create.py
@@ -1,5 +1,17 @@
 import os, sqlite3, pytest
+
 import chainreader.blocksDB_create as DBcreate
+from .test_tps import sendMoney_andWaitForReceipt
+
+###############################################################
+# web3 connection 
+
+from config import RPCaddress
+from clienttools import web3connection
+answer = web3connection(RPCaddress=RPCaddress)
+global w3, NODENAME, NODETYPE, CONSENSUS, NETWORKID, CHAINNAME, CHAINID 
+w3, chainInfos  = answer
+NODENAME, NODETYPE, CONSENSUS, NETWORKID, CHAINNAME, CHAINID = chainInfos
 
 # current path one up?
 # unfortunately path if different depending on how py.test is called
@@ -8,7 +20,9 @@ if os.path.split(path)[-1]=="tests":
     os.chdir("..") 
 
 ###############################################################
-# fixtures for the temporary throwaway database
+# for the temporary throwaway database
+# TODO: how to delete at the end of all tests?
+
 def set_DB_throwaway():
     DBcreate.DBFILE = os.path.join("tests", "testing_TEMP.db") 
     DBcreate.DB_dropTable()
@@ -94,7 +108,7 @@ def test_DB_writeRow_duplicate():
     
 def test_writeRowIntoFile():
     set_DB_throwaway()
-    DBcreate.writeRowIntoFile(block_example)
+    DBcreate.writeRowSQLIntoFile(block_example)
     
     assert True # no return value; just check that it raised no exception
 
@@ -104,7 +118,7 @@ def test_SQLfileIntoDB():
     delete_DB_and_SQL_file()
     
     DBcreate.DB_createTable()
-    DBcreate.writeRowIntoFile(block_example)
+    DBcreate.writeRowSQLIntoFile(block_example)
     
     conn = sqlite3.connect(DBcreate.DBFILE)
     DBcreate.SQLfileIntoDB(conn)
@@ -116,8 +130,8 @@ def test_SQLfileIntoDB_duplicates_caught():
     delete_DB_and_SQL_file()
     
     DBcreate.DB_createTable()
-    DBcreate.writeRowIntoFile(block_example)
-    DBcreate.writeRowIntoFile(block_example)
+    DBcreate.writeRowSQLIntoFile(block_example)
+    DBcreate.writeRowSQLIntoFile(block_example)
     
     conn = sqlite3.connect(DBcreate.DBFILE)
     DBcreate.SQLfileIntoDB(conn)
@@ -189,24 +203,76 @@ def test_DB_blocknumberMinMax():
     conn.close()    
 
 
-# unready
+def test_getBlock():
+    DBcreate.w3 = w3
+    blockNumber = 0
+    answer = DBcreate.getBlock(blockNumber)
+    print (answer)
+    assert answer["number"] == blockNumber
+    assert 'gasLimit' in answer
+    assert 'difficulty' in answer
+    
+
+def test_getBlock_then_store():
+    blockNumber = 0
+    DBcreate.getBlock_then_store(blockNumber)
+    assert True # no return value; just check that it raised no exception
 
 
+def test_multithreadedQueue_block0():
+    DBcreate.multithreadedQueue(0, 1, 10)
+    assert True # no return value; just check that it raised no exception
 
 
+def createBlocks():
+    sendMoney_andWaitForReceipt(10)
+    
+    currentBlockNumber = w3.eth.blockNumber
+    print ("currentBlockNumber", currentBlockNumber)
+    # return either toBlock=20 or if chain shorter then =chain_tip
+    toBlock = 8 if currentBlockNumber>=8 else currentBlockNumber
+    print ("toBlock", toBlock)
+    return toBlock
+  
+
+def test_multithreadedQueue_blocks():
+    set_DB_throwaway()
+    delete_DB_and_SQL_file()
+
+    toBlock = createBlocks()
+    DBcreate.multithreadedQueue(0, toBlock, 10)
+    assert True # no return value; just check that it raised no exception
 
 
+def test_manyBlocks_multithreaded():
+    set_DB_throwaway()
+    delete_DB_and_SQL_file()
+    
+    toBlock = createBlocks()
+    DBcreate.manyBlocks_multithreaded(0, toBlock)
+    assert True # no return value; just check that it raised no exception
 
 
+def test_manyBlocks_singlethreaded():
+    set_DB_throwaway()
+    delete_DB_and_SQL_file()
+    
+    toBlock = createBlocks()
+    DBcreate.manyBlocks_singlethreaded(0, toBlock)
+    assert True # no return value; just check that it raised no exception
+    
 
-
-
-
-
-
-
-
+def test_DB_newFromFile():
+    set_DB_throwaway()
+    delete_DB_and_SQL_file()
+    
+    toBlock = createBlocks()
+    DBcreate.manyBlocks_singlethreaded(0, toBlock)
+    
+    DBcreate.DB_newFromFile()
+    assert True # no return value; just check that it raised no exception
+    
     
 def test_CLI_params():
     DBcreate.CLI_params()
-        
+    assert True # How to test this, any idea?
