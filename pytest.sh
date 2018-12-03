daef283130c7aa202b5912a28cc4e844710656be
Andreas Krueger <coding@AndreasKrueger.de>
2018-12-03 16:02:04 +0000

    show coverage only for my files not for imported libraries

--- pytest.sh
+++ pytest.sh
@@ -14,7 +14,7 @@ echo you can watch it with tail -f tests/logs/testrpc-py.log
 echo kill it with: kill $PID
 
 # start tests, colored output to screen AND to file
-unbuffer py.test -v | tee tests/logs/py.test.log 
+unbuffer py.test -v --cov | tee tests/logs/py.test.log 
 
 echo
 echo done.
@@ -24,6 +24,6 @@ kill $PID
 echo should be gone now:
 ps aux | grep testrpc-py | grep -v grep
 echo
-echo see tests/logs/ for the log files of this test
+echo see tests/logs/ for the log files of this test:
 echo cat tests/logs/*
 echo
