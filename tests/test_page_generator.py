52fc3ab90e0e9aae447bf7bd277d7802702d3a28
Andreas Krueger <coding@AndreasKrueger.de>
2019-01-14 15:38:43 +0100

    markdown_generator.py renamed to page_generator.py

--- tests/test_page_generator.py
+++ tests/test_page_generator.py
@@ -10,7 +10,7 @@
 
 import os, json
 from hammer.config import RPCaddress, FILE_CONTRACT_SOURCE, FILE_LAST_EXPERIMENT
-import reader.markdown_generator as mg
+import reader.page_generator as pg
 
 
 ################################################################################
@@ -53,48 +53,48 @@ def dummy_infofile():
 def test_readInfofile():
     # so that the file exists:
     dummy_infofile()
-    data = mg.read_infofile(FILE_LAST_EXPERIMENT)
+    data = pg.read_infofile(FILE_LAST_EXPERIMENT)
     assert data['send']['num_txs'] == 10000
     
     
 def test_format_infofile_content():
-    T = mg.format_infofile_content(INFO_EXAMPLE)
+    T = pg.format_infofile_content(INFO_EXAMPLE)
     assert type(T) == type (" ")
     
     
 def test_readTpsLog():
-    mg.readTpsLog(fn=FILE_LAST_EXPERIMENT) # N.B.: Wrong file, but doesn't matter.
+    pg.readTpsLog(fn=FILE_LAST_EXPERIMENT) # N.B.: Wrong file, but doesn't matter.
     
     
 def test_title():
-    T=mg.title(INFO_EXAMPLE)
+    T=pg.title(INFO_EXAMPLE)
     assert T=="Geth v1.8.14-stable-316fc7ec with 10000 txs: 162.4 TPS"
     
     
 def test_save_page():
-    answer = mg.save_page("hello world", "testing.txt", folder=".")
+    answer = pg.save_page("hello world", "testing.txt", folder=".")
     os.remove(answer)
     assert answer=="./testing.txt"
     
     
 def test_timestamp_humanreadable():
-    answer = mg.timestamp_humanreadable(0)
+    answer = pg.timestamp_humanreadable(0)
     # in my case the full answer is:     19700101-0100 = why 01:00 am ?
     # --> only check beginning because local timezones:
     assert answer[0:2] == "19" 
     
 
 def test_filename():
-    assert mg.filename(INFO_EXAMPLE) == "Geth_20190114-1519_txs10000"
+    assert pg.filename(INFO_EXAMPLE) == "Geth_20190114-1519_txs10000"
     
     
 def test_createElements():
-    answers = mg.createElements(INFO_EXAMPLE, FILE_LAST_EXPERIMENT) # N.B.: Wrong file, but doesn't matter.
+    answers = pg.createElements(INFO_EXAMPLE, FILE_LAST_EXPERIMENT) # N.B.: Wrong file, but doesn't matter.
     assert answers[3] == "img/TEMP-20190114-1519_blks10-40.png"
     
     
 def test_makeAndSave_MarkdownPage():
-    answer = mg.makeAndSave_MarkdownPage(INFO_EXAMPLE, "title", "info", 
+    answer = pg.makeAndSave_MarkdownPage(INFO_EXAMPLE, "title", "info", 
                                          "tpslog", "image_location",
                                          runs_folder=".")
     os.remove(answer)
@@ -102,7 +102,7 @@ def test_makeAndSave_MarkdownPage():
 
 
 def test_makeAndSave_HTMLPage():
-    answer = mg.makeAndSave_HTMLPage(INFO_EXAMPLE, "title", "info", 
+    answer = pg.makeAndSave_HTMLPage(INFO_EXAMPLE, "title", "info", 
                                          "tpslog", "image_location",
                                          runs_folder=".")
     os.remove(answer)
@@ -111,7 +111,7 @@ def test_makeAndSave_HTMLPage():
 
 def test_CLI_params():
     try:
-        mg.CLI_params()
+        pg.CLI_params()
     except SystemExit:
         pass
     
