6480318b8cf34519d95037cf5122deac380c34f4
Andreas Krueger <coding@AndreasKrueger.de>
2019-01-14 15:36:12 +0100

    generate human readable pages from essential experiment information

--- reader/markdown_generator.py
+++ reader/markdown_generator.py
@@ -15,7 +15,7 @@
 ## Dependencies:
 
 # standard library:
-import sys, os, json
+import sys, os, json, time
 from pprint import pprint
 
 # pypi:
@@ -30,25 +30,204 @@ from hammer.config import FILE_LAST_EXPERIMENT
 ################################################################################
 
 def read_infofile(fn=FILE_LAST_EXPERIMENT):
+    """
+    reads in json formatted file, returns dict
+    """
     with open(fn, "r") as f:
         data = json.load(f)
     return data
 
 
+def format_infofile_content(info):
+    """
+    presents content of infofile as text block
+    """
+    
+    T=""
+    
+    i=info['node']
+    T+="NODE: %s on %s\n" % (i['web3.version.node'], i['rpc_address'])
+    T+="      consensus=%s chain_name=%s chain_id=%s network_id=%s\n"
+    T=T % (i['consensus'], i['chain_name'], i['chain_id'], i['network_id'])
+    
+    i=info['send']
+    T+="SEND: %d transactions in blocks %d-%d with %d empty blocks following.\n"
+    T=T % (i['num_txs'], i['block_first'], i['block_last'], i['empty_blocks'])
+    T+="      A sample of transactions looked as if they: "
+    T+="succeeded." if i['sample_txs_successful'] else "failed (at least partially)."
+    T+="\n"
+    
+    i=info['tps']
+    T+="TPS:  The stopclock watcher measured a final TPS of %.1f since contract deploy,\n" % i['finalTpsAv']
+    T+="      and in between saw values as high as %.1f TPS.\n" % i['peakTpsAv'] 
+    
+    i=info['diagrams']
+    T+="DIAG: Looking only at the experiment block-timestamps, the overall TPS is ~%.1f\n" % i['blocktimestampsTpsAv']
+    T+="      The diagrams were saved into '%s'.\n" % i['filename']
+    T+="      The experiment was prefixed '%s'." % i['prefix']
+    
+    return T
+
+
+def readTpsLog(fn):
+    """
+    read in a file, line by line
+    """
+    T = ""
+    with open(fn, "r") as f:
+        lines = f.readlines()
+        # print ("log len:", len(lines))
+        for line in lines:
+            T += line
+    return T
+
+
+def title(info):
+    """
+    concatenate page title from info dict
+    """
+    T = "%s %s with %d txs: %.1f TPS"
+    T = T % (info['node']['name'], info['node']['version'], 
+             info['send']['num_txs'], 
+             info['diagrams']['blocktimestampsTpsAv'])  
+    return T
+
+
+def createElements(info, TPSLOG):
+    """
+    make title, infoblock, log, diagram-location
+    """
+    
+    txt_info = format_infofile_content(info)
+    #print (txt_info); print()
+    
+    txt_tpslog = readTpsLog(TPSLOG)
+    # print (txt_tpslog)
+    
+    txt_title = title(info)
+    # print (txt_title)
+    
+    image_location = info['diagrams']['filename']
+    
+    return txt_title, txt_info, txt_tpslog, image_location 
+
+
+def save_page(body, fn, folder="../results/runs/"):
+    """
+    saves text to file
+    """
+    target = os.path.join(folder, fn)
+    with open(target, "w") as f:
+        f.write(body)
+    return target
+
+
+def timestamp_humanreadable(epoch):
+    """
+    epoch 0 <=> 1970-01-01
+    """
+    return time.strftime("%Y%m%d-%H%M", time.localtime(epoch))
+
+
+def filename(info):
+    """
+    filename, ~unique because it contains the experiment start minute
+    """
+    epoch = info['tps']['start_epochtime']
+    #print (epoch)
+    ts = timestamp_humanreadable(epoch)
+    fn = "%s_%s_txs%s" % (info['node']['name'], ts, info['send']['num_txs'])
+    return fn
+
+
+
+template01 = '''
+## %s
+
+### information:
+```
+%s
+```
+
+### log:
+```
+%s```
+
+### diagrams:
+![%s](%s)
+'''
+
+def makeAndSave_MarkdownPage(infodict, title, info, tpslog, image_location,
+                             runs_folder="../results/runs/"):
+    """
+    using the above template01, save a markdown page
+    """
+    
+    img_path = "../../reader/" + image_location
+    page = template01 % (title, info, tpslog, image_location, img_path)
+    # print(); print (page)
+    return save_page(page, filename(infodict)+".md", folder=runs_folder)
+
+    
+template02 = '''
+<html>
+<head><title>%s</title></head>
+<body>
+
+<h2>%s</h2>
+
+<h3>information:</h3>
+<PRE>%s</PRE>
+
+<h3>log:</h3>
+<PRE>%s</PRE>
+
+<h3>diagrams:</h3>
+%s
+<img src="%s"/>
+
+
+</body>
+</html>
+'''
+
+def makeAndSave_HTMLPage(infodict, title, info, tpslog, image_location,
+                         runs_folder="../results/runs/"):
+    """
+    using the above template02, save an HTML page
+    """
+    img_path = "../../reader/" + image_location
+    page = template02 % (title,title, info, tpslog, image_location, img_path)
+    # print(); print (page)
+    return save_page(page, filename(infodict)+".html", folder=runs_folder)
+
+
 def CLI_params():
-    if len(sys.argv)!=2:
-        print ("Please give one argument, the filename of the experiment-infofile.")
+    """
+    paths&files are handed over via CLI argument
+    """
+    if len(sys.argv)!=3:
+        print ("Please give two arguments, the filename of the experiment-infofile, and the tps.py logfile.")
         exit()
-    if len(sys.argv)==2:
+    else:
         INFOFILE=sys.argv[1]
-        print ("Reading from INFOFILE ", INFOFILE)
-    return INFOFILE
+        TPSLOG  =sys.argv[2] 
+        print ("Reading from INFOFILE %s and from TPSLOG %s" % (INFOFILE, TPSLOG))
+    return INFOFILE, TPSLOG
 
 
 if __name__ == '__main__':
-    INFOFILE = CLI_params()
-    data = read_infofile(INFOFILE)
-    pprint (data)
-    print ()
     
-        
+    INFOFILE, TPSLOG = CLI_params()
+    
+    info = read_infofile(INFOFILE)
+    pprint (info); print(); exit()
+    
+    elem = createElements(info, TPSLOG)
+    
+    fn = makeAndSave_MarkdownPage(info, *elem)
+    print ("Page saved to: ", fn)
+    fn = makeAndSave_HTMLPage(info, *elem)
+    print ("Page saved to: ", fn)
+    
+    
--- tests/test_markdown_generator.py
+++ tests/test_markdown_generator.py
@@ -22,18 +22,93 @@ if os.path.split(path)[-1]=="tests":
 
 ################################################################################
 
+INFO_EXAMPLE={'diagrams': {'blocktimestampsTpsAv': 162.36666666666667,
+              'filename': 'img/TEMP-20190114-1519_blks10-40.png',
+              'prefix': 'TEMP'},
+ 'node': {'chain_id': 326432352,
+          'chain_name': '???',
+          'consensus': 'clique',
+          'name': 'Geth',
+          'network_id': 500,
+          'rpc_address': 'http://localhost:8545',
+          'type': 'Geth',
+          'version': 'v1.8.14-stable-316fc7ec',
+          'web3.version.node': 'Geth/v1.8.14-stable-316fc7ec/linux-amd64/go1.10.3'},
+ 'send': {'block_first': 10,
+          'block_last': 40,
+          'empty_blocks': 10,
+          'num_txs': 10000,
+          'sample_txs_successful': True},
+ 'tps': {'finalTpsAv': 156.10037659498764,
+         'peakTpsAv': 160.8096672375769,
+         'start_epochtime': 1547475588.1457317},
+ 'ATTENTION' : "not real, file created by unittests!"}
+
 def dummy_infofile():
-    dummy = "not real, file created by tests/test_send.py"
-    data = {"send": {"num_txs" : dummy}}
+    data = INFO_EXAMPLE
     with open(FILE_LAST_EXPERIMENT, "w") as f:
         json.dump(data, f)
+
 
 def test_readInfofile():
     # so that the file exists:
     dummy_infofile()
-    mg.read_infofile(FILE_LAST_EXPERIMENT)
+    data = mg.read_infofile(FILE_LAST_EXPERIMENT)
+    assert data['send']['num_txs'] == 10000
     
     
+def test_format_infofile_content():
+    T = mg.format_infofile_content(INFO_EXAMPLE)
+    assert type(T) == type (" ")
+    
+    
+def test_readTpsLog():
+    mg.readTpsLog(fn=FILE_LAST_EXPERIMENT) # N.B.: Wrong file, but doesn't matter.
+    
+    
+def test_title():
+    T=mg.title(INFO_EXAMPLE)
+    assert T=="Geth v1.8.14-stable-316fc7ec with 10000 txs: 162.4 TPS"
+    
+    
+def test_save_page():
+    answer = mg.save_page("hello world", "testing.txt", folder=".")
+    os.remove(answer)
+    assert answer=="./testing.txt"
+    
+    
+def test_timestamp_humanreadable():
+    answer = mg.timestamp_humanreadable(0)
+    # in my case the full answer is:     19700101-0100 = why 01:00 am ?
+    # --> only check beginning because local timezones:
+    assert answer[0:2] == "19" 
+    
+
+def test_filename():
+    assert mg.filename(INFO_EXAMPLE) == "Geth_20190114-1519_txs10000"
+    
+    
+def test_createElements():
+    answers = mg.createElements(INFO_EXAMPLE, FILE_LAST_EXPERIMENT) # N.B.: Wrong file, but doesn't matter.
+    assert answers[3] == "img/TEMP-20190114-1519_blks10-40.png"
+    
+    
+def test_makeAndSave_MarkdownPage():
+    answer = mg.makeAndSave_MarkdownPage(INFO_EXAMPLE, "title", "info", 
+                                         "tpslog", "image_location",
+                                         runs_folder=".")
+    os.remove(answer)
+    assert answer == "./Geth_20190114-1519_txs10000.md"
+
+
+def test_makeAndSave_HTMLPage():
+    answer = mg.makeAndSave_HTMLPage(INFO_EXAMPLE, "title", "info", 
+                                         "tpslog", "image_location",
+                                         runs_folder=".")
+    os.remove(answer)
+    assert answer == "./Geth_20190114-1519_txs10000.html"
+
+
 def test_CLI_params():
     try:
         mg.CLI_params()
