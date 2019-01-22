e4b5d335c85c0bd2c73b82b12f0ba232b54bf573
Andreas Krueger <coding@AndreasKrueger.de>
2019-01-23 00:07:38 +0100

    when you interrupt the whole thing, you might need this

--- scripts/kill-leftovers.sh
+++ scripts/kill-leftovers.sh
@@ -2,4 +2,12 @@ sudo pkill docker-proxy
 sudo pkill bootnode
 sudo pkill docker-compose
 
+echo
+echo you might have to manually kill more:
+
+sudo scripts/netstat_port8545.sh
+
+echo
+ps aux | grep tps.py | grep -v grep
+
 
