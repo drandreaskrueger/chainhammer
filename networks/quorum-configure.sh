49a7d78543b9f26e9839286c7f8c73851a18ca52
Andreas Krueger <coding@AndreasKrueger.de>
2019-01-23 00:09:00 +0100

    nice: blk-io merged my patches into their master

--- networks/quorum-configure.sh
+++ networks/quorum-configure.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-echo TODO
-echo
-echo see merge request https://github.com/blk-io/crux/pull/29
-echo  
-echo until then do this manually, see
-echo https://github.com/drandreaskrueger/chainhammer/blob/master/docs/reproduce.md#quorum-crux-ibft
-echo 
+FOLDER=networks/repos/blk-io_crux/docker/quorum-crux
+GETBACK=../../../../..
 
+echo configure quorum
+
+cd $FOLDER
+
+echo git checkout a version that definitely worked and included my patches
+echo git checkout eeb63a91b7eda0180c8686f819c0dd29c0bc4d46
+git checkout eeb63a91b7eda0180c8686f819c0dd29c0bc4d46
+
+# nice: blk-io merged my patches into 
+# https://github.com/blk-io/crux/commit/ddd126e62f1f0dac341b2b61b01688f1d960dbd2
+# echo patch: raise gasLimit
+# jq '.gasLimit = "0x1312D00"' istanbul-genesis.json > tmp && mv tmp istanbul-genesis.json
+# echo patch: larger txpool, blockperiod 1 second
+# sed -i 's/PRIVATE_CONFIG/ARGS=$ARGS"--txpool.globalslots 50000 --txpool.globalqueue 50000 --istanbul.blockperiod 1 "\nPRIVATE_CONFIG/g' istanbul-start.sh
+
+echo patch: port 22001 change to 8545
+sed -i 's/22001:22000/8545:22000/g' docker-compose-local.yaml
+
+cd $GETBACK
--- scripts/install-network-starters.sh
+++ scripts/install-network-starters.sh
@@ -13,22 +13,27 @@ cd $FOLDER
 
 echo 
 echo paritytech/parity-deploy
-rm -rf paritytech_parity-deploy
+echo need sudo to remove parity-deploy folder, sorry their decision not mine:
+sudo rm -rf paritytech_parity-deploy
 git clone https://github.com/paritytech/parity-deploy.git paritytech_parity-deploy
+cd paritytech_parity-deploy
 git checkout 1a6afd17ac75bdf6c9e9fefa1d3af13748dd9cfa
+cd ..
 
 echo
 rm -rf drandreaskrueger_geth-dev
 echo javahippie/geth-dev my configured fork drandreaskrueger/geth-dev 
 git clone https://github.com/drandreaskrueger/geth-dev.git drandreaskrueger_geth-dev
+cd drandreaskrueger_geth-dev
 echo TODO: Perhaps instead clone from upstream and patch with chainhammer-specific parameters?
+cd ..
 
 echo 
 echo blk-io/crux
 rm -rf blk-io_crux
 git clone  https://github.com/blk-io/crux blk-io_crux
 cd blk-io_crux
-git checkout f39db2345cf9d82e76d3905468e6e5ea1469b09d
+git checkout eeb63a91b7eda0180c8686f819c0dd29c0bc4d46
 cd ..
 echo TODO: Perhaps already patch with chainhammer-optimized parameters?
 
