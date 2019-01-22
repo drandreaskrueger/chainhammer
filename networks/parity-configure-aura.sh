1c7f24e774bb632a25b7b839e37a8f473333fcf4
Andreas Krueger <coding@AndreasKrueger.de>
2019-01-22 22:56:26 +0100

    strange naming for the deployment/1/ folder in instantseal

--- networks/parity-configure-aura.sh
+++ networks/parity-configure-aura.sh
@@ -2,4 +2,5 @@
 
 PARITY_ARGS="--nodes 4 --config aura --geth --gasprice 0 --tx-queue-mem-limit 0 --tx-queue-size 32768 --force-sealing"
 PARITY_CONSENSUS=aura
-source networks/parity-configure.sh
+FIRST_NODE=1
+source networks/parity-configure.sh $1
--- networks/parity-configure-instantseal.sh
+++ networks/parity-configure-instantseal.sh
@@ -2,4 +2,8 @@
 
 PARITY_ARGS="--config dev --geth --gasprice 0 --tx-queue-mem-limit 0 --tx-queue-size 32768 --force-sealing"
 PARITY_CONSENSUS=instantseal
-source networks/parity-configure.sh
+
+# very strange naming for the folder, instead of deployment/1/ this is deployment/is_authority/ 
+FIRST_NODE=is_authority
+
+source networks/parity-configure.sh $1
