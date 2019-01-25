46ecadc5b846760284ee19d98d11ebabb7be01eb
Andreas Krueger <coding@AndreasKrueger.de>
2019-01-25 20:57:40 +0100

    omit warning and press enter, if arg=silent

--- scripts/remove-all-docker.sh
+++ scripts/remove-all-docker.sh
@@ -1,12 +1,17 @@
 
-echo
-echo kill and remove all docker containers and instances
-echo careful!
-echo especially if you still need the DATA in your docker containers
-echo this could turn out to be tragic
-echo 
-read -p "Press enter to continue"
-echo
+if [ "$1" == "silent" ]; then
+    echo Skipping warning - will directly remove all docker containers and images!
+    echo 
+else
+    echo
+    echo kill and remove all docker containers and instances
+    echo careful!
+    echo especially if you still need the DATA in your docker containers
+    echo this could turn out to be tragic
+    echo 
+    read -p "Press enter to continue"
+    echo
+fi
  
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
