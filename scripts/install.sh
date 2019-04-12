0a9453e0d166d1794ee00f62e48b0b20df15887f
Andreas Krueger <coding@AndreasKrueger.de>
2019-04-12 16:29:45 +0200

    docker, docker-compose, and 3 network starters can now be omitted!

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,13 +18,20 @@ trap 'echo; echo "\"${last_command}\" command filed with exit code $?."' EXIT
 # Thanks.
 #
 #
-#
-#
-#
+
+function install_chapter {
+    echo 
+    echo ==========================================
+    echo = $1
+    echo ==========================================
+        
+    source $1
+}
+
 echo 
 echo ===================================================================
 echo Install ChainHammer dependencies, and clone network starter repos
-echo version v55
+echo version v58
 echo ===================================================================
 echo 
 echo Please report any issues IF this script is NOT ending with: 
@@ -32,9 +39,13 @@ echo ... with exit code 0.
 echo
 echo Warning: No guarantees!! 
 echo Executing this script has consequences for this machine.
-echo 
 echo Better only use on a disposable/cloud/virtualbox machine, 
 echo and NOT on your main work machine!!
+echo
+if [ "$1" == "nodocker" ]; then
+    echo Your CLI switch: Do NOT install docker, and network starters!
+    echo
+fi
 echo 
 echo Is this the \"chainhammer\" folder where you want everything installed?
 echo $(pwd)
@@ -46,31 +57,35 @@ sudo ls scripts/install-*.sh
 echo got sudo, thanks.
 
 
-function install_chapter {
-    echo 
-    echo ==========================================
-    echo = $1
-    echo ==========================================
-        
-    source $1
-}
+#
+# Here is where the magic happens:
+#
 
+# sudo apt install ...
 install_chapter scripts/install-packages.sh
 
-install_chapter scripts/install-docker.sh
+# parity-deploy, geth-dev, and quorum-crux:
+if [ "$1" == "nodocker" ]; then
+    echo ====================================================== 
+    echo Skipping docker, docker-compose, and network-starters.
+    echo ====================================================== 
+else
+    install_chapter scripts/install-docker.sh
+    install_chapter scripts/install-docker-compose.sh
+    install_chapter scripts/install-network-starters.sh
+fi
 
-install_chapter scripts/install-docker-compose.sh
-
+# solc    
 install_chapter scripts/install-solc.sh
 
+# geth
 install_chapter scripts/install-geth.sh
 
-install_chapter scripts/install-network-starters.sh
-
+# env
 install_chapter scripts/install-virtualenv.sh
 
+# deploy.py andtests on testRPC
 install_chapter scripts/install-initialize.sh
-
 
 
 echo
