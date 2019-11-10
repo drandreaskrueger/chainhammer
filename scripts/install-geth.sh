6d0abbee44515ca047791e74619974a96fea67f2
Andreas Krueger <coding@AndreasKrueger.de>
2019-11-10 19:42:21 +0100

    newer go newer geth, and fix specific version

--- scripts/install-geth.sh
+++ scripts/install-geth.sh
@@ -1,5 +1,7 @@
+GETH_VERSION=v1.9.6
+
 echo
-echo compile and install geth
+echo compile and install geth $GETH_VERSION
 echo 
 read -p "Press enter to continue"
 
@@ -14,7 +16,25 @@ go version
 
 echo get, compile and install geth - patience please
 echo
-go get -d github.com/ethereum/go-ethereum
-go install "github.com/ethereum/go-ethereum/cmd/geth"
+
+echo download repo into $GOPATH
+go get -d -u github.com/ethereum/go-ethereum
+echo
+
+# set to a specific version
+echo checkout repo to specific geth version $GETH_VERSION
+OLDPATH=$(pwd)
+cd $GOPATH/src/github.com/ethereum/go-ethereum/
+git checkout $GETH_VERSION
+cd $OLDPATH
+echo	
+
+echo remove the old geth binary
+rm -f $(which geth)
+
+echo force rebuild geth command ethereum/go-ethereum/cmd/geth
+go clean -r "github.com/ethereum/go-ethereum/cmd/geth"
+go install  "github.com/ethereum/go-ethereum/cmd/geth"
+
 geth version
 
--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -1,7 +1,8 @@
-GO_INSTALLER="go1.12.6.linux-amd64.tar.gz"
+GO_INSTALLER="go1.13.4.linux-amd64.tar.gz"
 
 echo
 echo installing golang version $GO_INSTALLER
+echo NB: any problems, try to move folder /usr/local/go and examine ~/.profile
 echo
 read -p "Press enter to continue"
 
@@ -11,10 +12,12 @@ rm -f $GO_INSTALLER
 wget https://dl.google.com/go/$GO_INSTALLER
 sudo tar -C /usr/local -xzf $GO_INSTALLER
 rm $GO_INSTALLER
+
+echo creating folder ~/go and adding to ENV
 mkdir -p ~/go/bin
 echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.profile
+echo "export GOPATH=~/go" >> ~/.profile
 export PATH=$PATH:/usr/local/go/bin:~/go/bin
+export GOPATH=~/go
+
 go version
-
-
-
