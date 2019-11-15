9f8a6863111a9d883a1fd4c6537f60018c41b1e0
Andreas Krueger <coding@AndreasKrueger.de>
2019-11-15 11:50:52 +0100

    substrate-starter is now a standalone repo

--- networks/node-template_purge-4-local-nodes.sh
+++
@@ -1,8 +0,0 @@
-
-echo Be aware this might cause problems if nodes are still running.
-echo
-node-template purge-chain -y --chain=local --base-path /tmp/alice
-node-template purge-chain -y --chain=local --base-path /tmp/bob
-node-template purge-chain -y --chain=local --base-path /tmp/charlie
-node-template purge-chain -y --chain=local --base-path /tmp/dave
-
